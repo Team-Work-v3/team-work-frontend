1e6cee0261322509ba5efd7462df44e1755db114
svbdz <denissvbdz@gmail.com>
2025-10-12 17:09:27 +0300

    validation fix

--- login.js
+++ login.js
@@ -2,69 +2,74 @@
 let correct_login = ''; //backend
 let correct_password = ''; //backend
 
-//Привязываем событие ВВОД к полю ЛОГИНА
-let login = document.getElementById("login");
-login.addEventListener('input', funct_login)
+//Привязываем события ФОКУСА и ПОТЕРИ ФОКУСА к полю ЛОГИНА
+login.addEventListener('focus', clear_login_message);
+login.addEventListener('blur', validate_login);
 
-//Привязываем функцию к событию
-function funct_login() {
+//функция очистки сообщения при фокусе
+function clear_login_message() {
+    check_login.innerHTML = '';
+}
+
+//функция проверки логина при потере фокуса
+function validate_login() {
     let value = login.value;
-    //проверка: от 5 до 20 символов, только буквы, цифры, подчёркивания
-    let login_regex = /^[a-zA-Zа-яА-Я0-9_]{5,20}$/;
+    let login_regex = /^[\wа-яА-Я]{5,20}$/;
 
     //если длина value() введенного поля меньше 5
-    if (value.length < 5) {
-        check_login.innerHTML = 'слишком короткий логин';
+    if (value.length < 2) {
+        check_login.innerHTML = 'Слишком короткий логин';
         check_login.style.color = 'red';
     }
     //если длина больше 20
-    else if (value.length > 20) {
-        check_login.innerHTML = 'слишком длинный логин';
+    else if (value.length > 50) {
+        check_login.innerHTML = 'Слишком длинный логин';
         check_login.style.color = 'red';
+    }
+    //если соответствует шаблону
+    else if (login_regex.test(value)) {
+        check_login.innerHTML = 'Логин принят';
+        check_login.style.color = 'green';
     }
     //если не соответствует шаблону
-    // восклицаиетльный знак нужен для проверки что условие не выполняется
-    else if (!login_regex.test(value)) {
-        check_login.innerHTML = 'логин содержит недопустимые символы';
-        check_login.style.color = 'red';
-    }
-    //если введенное не совпадает
     else {
-        check_login.innerHTML = '-';
-        check_login.style.color = '';
+        check_login.innerHTML = 'Используй только буквы, цифры и _';
+        check_login.style.color = 'red';
     }
 }
 
-//привязываем событие ВВОДА к полю ПАРОЛЯ
-password.addEventListener('input', funct_password)
+//Привязываем события ФОКУСА и ПОТЕРИ ФОКУСА к полю ПАРОЛЯ
+password.addEventListener('focus', clear_password_message);
+password.addEventListener('blur', validate_password);
 
-//функция проверки
-function funct_password() {
+//функция очистки сообщения при фокусе
+function clear_password_message() {
+    check_password.innerHTML = '';
+}
+
+//функция проверки пароля при потере фокуса
+function validate_password() {
     let value = password.value;
-    //проверка: от 6 до 30 символов, допускаются спецсимволы
     let password_regex = /^[a-zA-Zа-яА-Я0-9!@#\$%\^&\*\(\)_\+\-=]{6,30}$/;
 
     //если длина value(=содержимого) не меньше 3
     if (value.length < 6) {
-        check_password.innerHTML = 'слишком короткий пароль';
+        check_password.innerHTML = 'Слишком короткий пароль';
         check_password.style.color = 'red';
     }
     //если длина больше 30
     else if (value.length > 30) {
-        check_password.innerHTML = 'слишком длинный пароль';
+        check_password.innerHTML = 'Слишком длинный пароль';
         check_password.style.color = 'red';
     }
-    // восклицаиетльный знак нужен для проверки что условие не выполняется
+    //если соответствует шаблону
+    else if (password_regex.test(value)) {
+        check_password.innerHTML = 'Пароль принят';
+        check_password.style.color = 'green';
+    }
     //если не соответствует шаблону
-    else if (!password_regex.test(value)) {
-        check_password.innerHTML = 'пароль содержит недопустимые символы';
-        check_password.style.color = 'red';
-    }
-    //предыдущих ошибок не было
     else {
-        check_password.innerHTML = '-';
-        check_password.style.color = '';
+        check_password.innerHTML = 'Используй допустимые символы';
+        check_password.style.color = 'red';
     }
 }
-
-
