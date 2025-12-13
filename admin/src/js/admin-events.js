4e627c8790d2e03cd1a5ad94c24465b6f745089a
svbdz <svabodzin2@mail.ru>
2025-12-13 10:33:36 +0300

    events

--- admin/src/js/admin-events.js
+++ admin/src/js/admin-events.js
@@ -1,36 +1,10 @@
-AddJSON();
-RenderEvents(getData());
-getEvents();
+RenderEvents(getEvents());
 
 async function getEvents() {
     const response = await fetch("http://62.109.16.129:5000/api/getEvents");
     const events = await response.json();
-    console.log(events);
-}
-
-function AddJSON() {
-    const bool = localStorage.getItem("is-added") === "true";
-
-    if (!bool) {
-        const jsonData = {
-            "event": {
-                "1": { "id": 1, "name": "Фильм 1", "location": "Минск ул. Романовская Слобода 28", "active": true },
-                "2": { "id": 2, "name": "Фильм 2", "location": "Минск ул. ул. Притыцкого 23", "active": true },
-                "3": { "id": 3, "name": "Фильм 3", "location": "Минск пр. Независимости 13", "active": false },
-                "4": { "id": 4, "name": "Фильм 4", "location": "Минск пр. Дзержинского 104", "active": true },
-                "5": { "id": 5, "name": "Фильм 5", "location": "Минск ул. Бобруйская 6", "active": false }
-            }
-        };
-
-        localStorage.setItem("JSON", JSON.stringify(jsonData));
-        localStorage.setItem("is-added", "true");
-    }
-}
-
-function getData() {
-    const data = localStorage.getItem("JSON");
-
-    if (data) return JSON.parse(data);
+    console.log(events.events);
+    return events.events;
 }
 
 function RenderEvents(data) {
@@ -44,21 +18,21 @@ function RenderEvents(data) {
     Object.values(data).forEach(events => {
         Object.values(events).forEach(event => {
             const html = `
-                <div class="event-container" id="event-container" data-event-id="${event.id}">
+                <div class="event-container" id="event-container" data-event-id="${event.event_id}">
                     <div class="event-visible-information">
                         <div class="elements-event">
-                            <span class="arrow-event" id="arrow-event" data-event-id="${event.id}">⯆</span>
-                            <span class="name-event">${event.name}</span>
+                            <span class="arrow-event" id="arrow-event" data-event-id="${event.event_id}">⯆</span>
+                            <span class="name-event">${event.name_event}</span>
                         </div>
                         <div class="buttons-event">
                             <button class="button-event" id="edit-event-button" onclick="window.location.href = 'admin-edit-event.html'">Edit</button>
                             <button class="button-event" id="delete-event-button">Delete</button>
                         </div>
                     </div>
-                    <div class="event-invisible-information" id="invisible-information-${event.id}">
+                    <div class="event-invisible-information" id="invisible-information-${event.event_id}">
                         <div>
                             <span class="information-event">${event.location}</span>
-                            <span>${event.active === true ? "Активно" : "Неактивно"}</span>
+                            <span>${event.is_active === 1 ? "Активно" : "Неактивно"}</span>
                         </div>
                         <!-- In the future, this div is going img -->
                         <div class="picture-event"></div>
