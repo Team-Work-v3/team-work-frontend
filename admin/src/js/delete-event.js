e2fba467f737b8db9f93d9a5c19e4b00517d34a8
svbdz <svabodzin2@mail.ru>
2026-01-10 11:24:12 +0300

    delete-event.js

---
+++ admin/src/js/delete-event.js
@@ -0,0 +1,28 @@
+async function deleteEvent(id) { 
+    try {
+        const info = {
+            'id': id
+        };
+        let inf = JSON.stringify(info);
+        console.log("Отправка запроса на удаление ID:", id);
+
+        const response = await fetch(`http://62.109.16.129:5000/api/deleteEvent`, {
+            method: "POST", 
+            headers: {"Content-Type": "application/json"},
+            body: inf
+        });
+
+        const result = await response.json();
+
+        console.log("Результат удаления:", result);
+
+        if (response.ok) {
+            console.log(`Событие с ID ${id} успешно удалено.`);
+        } else {
+            console.error(`Ошибка при удалении события ID ${id}:`, result.message || response.statusText);
+        }
+    } catch (error) {
+        console.error("Ошибка при отправке запроса на удаление:", error);
+    }
+}
+
