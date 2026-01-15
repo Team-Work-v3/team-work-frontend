fd2d5c9253c08625e952083ebfd64e9590187ef7
svbdz <denissvbdz@gmail.com>
2026-01-15 21:26:59 +0300

    fix js and css admin

--- admin/src/js/admin-events.js
+++ admin/src/js/admin-events.js
@@ -38,15 +38,15 @@ async function RenderEvents() {
             </div>
 
                         <div id="special">
+                                                <div class="add-event-blocks">
+                            <label for="fullDescription-event">Обложка мероприятия</label>
+                            <img alt="picture" src="${event.images_events}" class="picture-event">
+                            </div>
                         <div class="add-event-blocks">
               <label for="fullDescription-event">Описание мероприятия (полное)</label>
               <textarea class="input--block" id="fullDescription_event" name="fullDescriptionevent" rows="" type="text"
                 oninput="fullDescriptionValidate(this)">${event.fullDescription_event}</textarea>
-                        
-                        <div class="add-event-blocks">
-                            <label for="fullDescription-event">Описание мероприятия (полное)</label>
-                            <img alt="picture" src="${event.images_events}" class="picture-event">
-                            </div>
+                      
                         </div>
                             
             </div>
