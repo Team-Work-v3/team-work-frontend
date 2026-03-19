53719f30d689d4014e18afb87d801ca8c3174f04
svbdz <denissvbdz@gmail.com>
2026-03-19 21:11:10 +0300

    fix

--- admin/src/js/members.js
+++ admin/src/js/members.js
@@ -51,7 +51,7 @@ RenderUsersInEvents();
                         
                              <tr>
                             
-                                <td>${count}</td><td>${user.full_name}</td><td>${user.email}</td><td>${user.phone_number}</td><td>${user.phone_number}</td><td><button onclick="">Удалить</button></td>
+                                <td>${count}</td><td>${user.full_name}</td><td>${user.email}</td><td>${user.phone_number}</td><td>-</td><td><button onclick="">Удалить</button></td>
                                 </tr>          
                                        
                         `;
