80f040e636a9f8662fb3002b53b3b12a92827147
ECHTP-57-02\User <it.academy.vs@gmail.com>
2026-03-27 19:20:28 +0300

    IIII

--- admin/src/js/reviews.js
+++ admin/src/js/reviews.js
@@ -8,8 +8,9 @@ RenderUsersInEvents();
             const response = await fetch("http://62.109.16.129:5000/api/getEvents?state=back");
             const data = await response.json();
             console.log(data.events);
-            
+
             for (const events of Object.values(data)) {
+                console.log(events.event_id);
                 const response2 = await fetch("http://62.109.16.129:5000/api/getAllReviews?ad=2");
                 const data2 = await response2.json();
                 console.log(data2.reviews);
