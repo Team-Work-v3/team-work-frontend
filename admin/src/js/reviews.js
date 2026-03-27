69c96bcd8a9d1fd5052aad3aa005d7c77985366e
ECHTP-57-02\User <it.academy.vs@gmail.com>
2026-03-27 19:13:47 +0300

    HZ

--- admin/src/js/reviews.js
+++ admin/src/js/reviews.js
@@ -3,80 +3,86 @@ RenderUsersInEvents();
         async function RenderUsersInEvents() {
             const container = document.querySelector(".events-list");
 
-            const response = await fetch("http://62.109.16.129:5000/api/getAllReviews");
+
+
+            const response = await fetch("http://62.109.16.129:5000/api/getEvents?state=back");
             const data = await response.json();
             console.log(data.events);
+            
+            // const response = await fetch("http://62.109.16.129:5000/api/getAllReviews");
+            // const data = await response.json();
+            // console.log(data.events);
 
-            if (!container) {
-                console.error("No data");
-                return;
-            }
+            // if (!container) {
+            //     console.error("No data");
+            //     return;
+            // }
 
-            Object.values(data).forEach(events => {
-                Object.values(events).forEach(event => {
+            // Object.values(data).forEach(events => {
+            //     Object.values(events).forEach(event => {
 
-                    var content = '';
-                    var inner_content = '';
-                    var html_start = `
+            //         var content = '';
+            //         var inner_content = '';
+            //         var html_start = `
 
-                        <div class="event-container" id="event-container" data-event-id="${event.event_id}">
-                            <div class="event-visible-information">
-                                <div class="elements-event">
-                                    <span class="arrow-event arrow" id="arrow-event" data-event-id="${event.event_id}">⯆</span>
-                                    <span class="name-event event-title">${event.event_name}</span>
-                                </div>
-                            </div>
-                            <div class="event-invisible-information" id="invisible-information-${event.event_id}">`;
+            //             <div class="event-container" id="event-container" data-event-id="${event.event_id}">
+            //                 <div class="event-visible-information">
+            //                     <div class="elements-event">
+            //                         <span class="arrow-event arrow" id="arrow-event" data-event-id="${event.event_id}">⯆</span>
+            //                         <span class="name-event event-title">${event.event_name}</span>
+            //                     </div>
+            //                 </div>
+            //                 <div class="event-invisible-information" id="invisible-information-${event.event_id}">`;
 
-                        if(event.users.length > 0){
-                         var html_center_head = `
-                                 <table class="participants-table">
-                                    <thead>
-                                        <tr>
-                                            <th class="th-number">№</th>
-                                            <th class="th-FIO">ФИ</th>
-                                            <th class="th-email">Содержание</th>
-                                            <th class="th-phnumber">Дата</th>
-                                            <th class="th-place"></th>
-                                            <th></th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>`;
+            //             if(event.users.length > 0){
+            //              var html_center_head = `
+            //                      <table class="participants-table">
+            //                         <thead>
+            //                             <tr>
+            //                                 <th class="th-number">№</th>
+            //                                 <th class="th-FIO">ФИ</th>
+            //                                 <th class="th-email">Содержание</th>
+            //                                 <th class="th-phnumber">Дата</th>
+            //                                 <th class="th-place"></th>
+            //                                 <th></th>
+            //                             </tr>
+            //                         </thead>
+            //                         <tbody>`;
                                         
-                        var center = ""; 
-                        var count = 0;
-                        Object.values(event.users).forEach(user => {
-                            count++;
-                        center += `
+            //             var center = ""; 
+            //             var count = 0;
+            //             Object.values(event.users).forEach(user => {
+            //                 count++;
+            //             center += `
                         
-                             <tr>
+            //                  <tr>
                             
-                                <td>${count}</td><td>${user_name}</td><td>${review_text}</td><td>${created_at}</td><td><button onclick="">Принять</button></td><td><button onclick="">Удалить</button></td>
-                                </tr>          
+            //                     <td>${count}</td><td>${user_name}</td><td>${review_text}</td><td>${created_at}</td><td><button onclick="">Принять</button></td><td><button onclick="">Удалить</button></td>
+            //                     </tr>          
                                        
-                        `;
-                    });
+            //             `;
+            //         });
 
-                var html_end = ` 
+            //     var html_end = ` 
                                        
-                                    </tbody>
-                                </table>
+            //                         </tbody>
+            //                     </table>
                          
-                    `;
-                 inner_content += (html_center_head + center + html_end);
-                }
-                else{
-                    inner_content += '<h2 class="container--h2">На это мероприятие ещё никто не зарегистировался</h2>';
-                }
+            //         `;
+            //      inner_content += (html_center_head + center + html_end);
+            //     }
+            //     else{
+            //         inner_content += '<h2 class="container--h2">На это мероприятие ещё никто не зарегистировался</h2>';
+            //     }
                     
-                    content = html_start + inner_content +' </div> </div>';
-                    container.innerHTML += content;    
-                 });
+            //         content = html_start + inner_content +' </div> </div>';
+            //         container.innerHTML += content;    
+            //      });
 
-                //  container.innerHTML += content;
-            });
+            //     //  container.innerHTML += content;
+            // });
 
-            OpenUsers();
+            // OpenUsers();
         }
 
         function OpenUsers() {
